0be68441a266c0e845bffd433cb2942772606f4d
Kurt Tito <kurtcedricktito@gmail.com>
2017-04-10 15:44:32 -0700

    Barebones
    Simple Main Menu Added
    -Singleplayerbutton
    -Mulltiplayerbutton
    -Settings
    SceneManager needs work
    DragHandler Script created but currently not functional
    Currently only card GameObject created
    FlipCard Method Works

---
+++ Illuminati Card Game 2D/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+    // Use this for initialization
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+
+    private bool dragging = false;
+    private float distance;
+
+    new SpriteRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseEnter()
+    {
+        renderer.material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
---
+++ Illuminati Card Game 2D/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject item;   
+    public GameObject card;
+    public Transform moveCard;
+
+    void Start()
+    {
+        moveCard = card.GetComponent<Transform>();
+    }
+
+    Transform startParent;
+    Vector3 startPosition;
+    //bool start = true;
+    //Sprite sprite;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        item = gameObject;
+        startPosition = transform.position;
+        //startParent = transform.parent;
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        moveCard.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        moveCard.position = Input.mousePosition;
+    }
+
+}
