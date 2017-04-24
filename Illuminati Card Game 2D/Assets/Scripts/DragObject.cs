1fd7150d9c6577c08815f6e5fa86d787e17aa674
Kurt Tito <kurtcedricktito@gmail.com>
2017-04-24 00:50:57 -0700

    Re: update

---
+++ Illuminati Card Game 2D/Assets/Scripts/DragObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+
+    float distances = 10;
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distances);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+}
