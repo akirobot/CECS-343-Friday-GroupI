ab491de8a6f29b02bf5ae5ba4a390e3f062d4011
Kurt Tito <kurtcedricktito@gmail.com>
2017-04-23 19:38:54 -0700

    Update
    +Opening Sequence Added
    +Quit Game Button Added and functional
    +Removed single player button
    +Changed multiplay button to Pass N Play
    +Opening/Title Screen Music Added
    +Some scripts added and edited
    +Illuminati executable added, tested and runs
    
    WIP:
    -Escape menu during play to access title screen again or quit
    application why playing
    -Die Objects/Roll Die function
    -Setup for varying number of players
    -Deck
    -ShuffleDeck
    -Draw card/drag and drop card
    -EndTurn/Change player turn

---
+++ Illuminati Card Game 2D/Assets/Scripts/OpeningAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningAudio : MonoBehaviour {
+
+    // Use this for initialization
+    new
+
+    // Use this for initialization
+    AudioSource audio;
+
+    void Start () {
+        audio = GetComponent<AudioSource>();
+        //audio.time = 1.25f;
+        audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if(audio.time > 9.50f)
+        //{
+        //    audio.Stop();
+        //}
+	}
+}
---
+++ Illuminati Card Game 2D/Assets/Scripts/OpeningLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("CountDown");
+	}
+	
+    private IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(10.25f);
+        Application.LoadLevel(1);
+        //SceneManager.LoadScene("Main Menu");
+    }
+}
