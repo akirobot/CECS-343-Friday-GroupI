ddfe5e24b68e53153a038ace00d264acf284d23a
Kurt Tito <kurtcedricktito@gmail.com>
2017-05-05 20:39:41 -0700

    REUPLOADED PROJECT

---
+++ Illuminati Card Game 2D/Assets/Scripts/CardModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel : MonoBehaviour {
+    //Initialize SpriteRender variable
+    //Used for accessing SpriteRender Component in Awake method
+    SpriteRenderer spriteRenderer;
+
+    //Array of faces; data type Sprite
+    //single back sprite
+    public Sprite[] faces;
+    public Sprite back;
+
+    //index for showing which card face
+    public int cardIndex;
+
+    //Flipcard method
+    public void flipCard(bool showFace)
+    {
+        if(showFace)
+        {   //if true, display faces[cardIndex]
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else
+        {   //else, display backside of card
+            spriteRenderer.sprite = back;
+        }
+    }
+
+    void Awake()
+    {   //get SpriteRenderer Component
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+}
