888ef768f17934259ad3c57618e3ff49ee7df645
Kurt Tito <kurtcedricktito@gmail.com>
2017-05-08 02:27:11 -0700

    Drag and View Cards
    +Cards are now draggable
    +Second Camera to view card up close(WIP)

--- Illuminati Card Game 2D/Assets/Scripts/CardModel.cs
+++ Illuminati Card Game 2D/Assets/Scripts/CardModel.cs
@@ -6,6 +6,8 @@ public class CardModel : MonoBehaviour {
     //Initialize SpriteRender variable
     //Used for accessing SpriteRender Component in Awake method
     SpriteRenderer spriteRenderer;
+    public Camera mainCamera;
+    public Camera cardCamera;
 
     //Array of faces; data type Sprite
     //single back sprite
@@ -31,6 +33,17 @@ public class CardModel : MonoBehaviour {
     void Awake()
     {   //get SpriteRenderer Component
         spriteRenderer = GetComponent<SpriteRenderer>();
+        mainCamera.enabled = true;
+        cardCamera.enabled = false;
     }
-    
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonUp(2))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            cardCamera.enabled = !cardCamera.enabled;
+        }
+    }
+
 }
---
+++ Illuminati Card Game 2D/Assets/Scripts/Next Turn Scripts/EnterToEndTurn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterToEndTurn : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Return))
+            transform.Rotate(Vector3.forward * 90);
+	}
+
+    public void BUTTON_ACTIONS_EndTurn()
+    {
+        transform.Rotate(Vector3.forward * 90);
+    }
+}
