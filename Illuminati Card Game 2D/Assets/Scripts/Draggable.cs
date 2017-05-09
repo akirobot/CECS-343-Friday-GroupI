81a5b0392e3248a3f8af45cb25ffe7a9feca5ebe
Calvin Vo <calvinctvo@gmail.com>
2017-05-08 19:35:44 -0700

    Add files via upload

---
+++ Illuminati Card Game 2D/Assets/Scripts/Draggable.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDragHandler
+{
+
+    public Transform parentToReturnTo = null;
+    public RectTransform Card;
+
+    void Start()
+    {
+        Card = GetComponent<RectTransform>();
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        Debug.Log("OnBeginDrag");
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+       // Debug.Log("OnDrag");
+        this.transform.position = eventData.position;
+       
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    public void RotateCard()
+    {
+        Debug.Log("Entered Card");
+        Card.Rotate(new Vector3(0, 0, 90));
+        //if (Input.GetKeyUp(KeyCode.A))
+        //{
+        //    Card.Rotate(new Vector3(0, 0, 90));
+        //    Debug.Log("Pressed A");
+        //}
+    }
+
+}
