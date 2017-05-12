67abaa586be10f99693a4e9c0bf7d336968becb3
Quoteful <jeffreyheng96@yahoo.com>
2017-05-11 17:30:05 -0700

    Add files via upload

---
+++ Illuminati Card Game 2D/Assets/Scripts/Jeffrey/SliderAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderAudio : MonoBehaviour {
+    public Slider audioslider;
+
+    //int x = audioslider.
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float volume = audioslider.value;
+        AudioListener.volume = volume;
+        Debug.Log(AudioListener.volume);
+	}
+}
